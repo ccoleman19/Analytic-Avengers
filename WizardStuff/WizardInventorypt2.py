7773997946576c3a3deab16da88a712ff0767bc5
Karieo <ccubed34@gmail.com>
2019-11-09 20:50:42 -0600

    adding final changes

--- WizardStuff/WizardInventorypt2.py
+++ WizardStuff/WizardInventorypt2.py
@@ -14,40 +14,70 @@ def openFile(fileName):
         with open(fileName, 'r') as inFile:
             wizardItems = [line.rstrip() for line in inFile]
             return wizardItems
-    except:
+    except FileNotFoundError:
         print("Could not find the items file.\nExiting program.  Bye!")
+    except Exception as e:
+        print(type(e),e)
 
-# print(openFile('wizard_all_items.txt'))
-inventory = openFile(fileName)
+items = openFile(fileName)
 
-def walk():
-    randomItem = random.choice(inventory)
-    print(f"While walking down a path, you see {randomItem}")
-    return randomItem
+def editItems(inventory):
+    with open('WizardStuff\wizardInventory.txt','w',newline="") as file:
+        for item in inventory:
+                file.write(item + "\n")
 
-# def grab():
-    
+def walk(inventory):
+    randomItem = random.choice(items)
+    print(f"While walking down a path, you see {randomItem}.")
+    grabItem = input('Do you want to grab it? (y/n): ').lower()
+    if grabItem == 'y':
+        grab(randomItem, inventory)
 
-def show(inventory: list):
-    index = 1
-    for item in inventory:
-        print(f"{index} {item}")
-        index +=1
+def grab(randomItem, inventory):
+    maxLimit = 4
+    if len(inventory) < maxLimit:
+        inventory.append(randomItem)
+        editItems(inventory)
+    else:
+        print("You can't carry any more items. Drop something first.")
+    return inventory
+
+def drop(inventory):
+    try:
+        index = int(input("number: "))
+        if index <= len(inventory):
+            index = index - 1
+            inventory.remove(inventory[index])
+            editItems(inventory)
+        else:
+            print('No item in that slot. Pick another!')
+    except ValueError:
+        print('Invalid item number')
+
+def show(inventory):
+    if len(inventory) != 0:
+        index = 1
+        for item in inventory:
+            print(f"{index} {item}")
+            index +=1
+    else:
+        print('You have no items')
 
 def main():
+    inventory = []
     menu()
     while True:
         command = input('Command: ')
         if command == 'walk':
-            walk()
+            walk(inventory)
         elif command == 'show':
-            print('show')
+            show(inventory)
         elif command == 'drop':
-            print('drop')
+            drop(inventory)
         elif command == 'exit':
             break
         else:
             print('Not a valid command. Please Try again!')
-            
+
 if __name__ == '__main__':
     main()
