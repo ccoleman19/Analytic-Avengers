7a1024d872fdca70245706dcdb7a9272d7b9383c
Karieo <ccubed34@gmail.com>
2019-11-17 17:46:42 -0600

    adding the final version of PigLatin Translator

--- PigLatin.py
+++ PigLatin.py
@@ -1,27 +1,64 @@
-
-
 import string
 
+
 def translateWord(textList):
+    newWords = []
     for word in textList:
-        if word[0] in ('a','e', 'i','o', 'u'):
-            newWord = word + 'way'
-            print(newWord)
-        
-    
+        if word[0] in "aeiou":
+            newWord = word + "way"
+            newWords.append(newWord)
+        else:
+            fVowel = firstVowel(word)
+            beginning = word[0:fVowel]
+            ending = word[fVowel:]
+            newWord = ending + beginning + "ay"
+            newWords.append(newWord)
+    return newWords
+
+
+def firstVowel(word):
+    try:
+        vowelList = []
+        for l in "aeiouy":
+            vowel = word.find(l)
+            if vowel > 0:
+                vowelList.append(vowel)
+        return min(vowelList)
+    except ValueError:
+        pass
+
 
 def wordList(text):
-    removePunctuation = str.maketrans('','', string.punctuation)
+    removePunctuation = str.maketrans("", "", string.punctuation)
+    text = text.translate(removePunctuation)
     textList = text.translate(removePunctuation).split()
-    print(translateWord(textList))
-    print(textList)
+    return textList, text
+
+
+def unpackList(listWords):
+    newSentance = ""
+    for word in listWords:
+        newSentance += word + " "
+    return newSentance
+
+
+def redo():
+    redo = input("Continue? (y/n): ").lower()
+    if redo == "y":
+        main()
+    else:
+        print("\n\nGood Bye!")
+
 
 def main():
-    print('Pig Latin Translator\n\n')
-    text = input('Enter Text:  ').lower()
-    wordList(text)
-    
+    print("Pig Latin Translator\n\n")
+    text = input("Enter Text:  ").lower()
+    print("English:     " + wordList(text)[1])
+    pLatinWords = translateWord(wordList(text)[0])
+    print("Pig Latin:   " + unpackList(pLatinWords))
+    print()
+    redo()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
